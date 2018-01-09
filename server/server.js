039a1d4dce11686c159949bd53118325520c05af
Hugo Saracino <hugo.saracino@gmail.com>
2018-01-09 21:08:13 +0100

    Added DELETE /todos/:id and tests

--- server/server.js
+++ server/server.js
@@ -55,6 +55,24 @@ app.post('/todos', (req, res) => {
     );
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo
+    .findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    });
+});
+
 app.listen(port, () => {
   console.log(`Started listening on port ${port}`)
 });
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -110,3 +110,31 @@ describe('POST /todos', () => {
   });
 
 });
+
+describe('DELETE /todos/:id', () => {
+
+  it('should return todo doc', done => {
+    request(app)
+      .delete(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', done => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for invalid object ids', done => {
+    request(app)
+      .delete('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
+
+});
