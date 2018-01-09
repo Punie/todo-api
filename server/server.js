238391426709b33039db1e19e3bdff6a8ec232b3
Hugo Saracino <hugo.saracino@gmail.com>
2018-01-09 02:06:19 +0100

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -15,7 +16,28 @@ app.get('/todos', (req, res) => {
     .then(
       todos => { res.send({ todos }); },
       err => { res.status(400).send(err); }
-    )
+    );
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo
+    .findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.post('/todos', (req, res) => {
@@ -28,7 +50,7 @@ app.post('/todos', (req, res) => {
     .then(
       doc => { res.send(doc); },
       err => { res.status(400).send(err); }
-    )
+    );
 });
 
 app.listen(3000, () => {
