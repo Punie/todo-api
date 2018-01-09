29d44f9c813c0a420d6404799cb0292c0f943954
Hugo Saracino <hugo.saracino@gmail.com>
2018-01-09 21:48:41 +0100

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -16,8 +17,8 @@ app.get('/todos', (req, res) => {
   Todo
     .find()
     .then(
-      todos => { res.send({ todos }); },
-      err => { res.status(400).send(err); }
+      todos => res.send({ todos }),
+      err => res.status(400).send(err)
     );
 });
 
@@ -37,9 +38,7 @@ app.get('/todos/:id', (req, res) => {
 
       res.send({ todo });
     })
-    .catch(err => {
-      res.status(400).send();
-    });
+    .catch(err => res.status(400).send());
 });
 
 app.post('/todos', (req, res) => {
@@ -50,9 +49,36 @@ app.post('/todos', (req, res) => {
   todo
     .save()
     .then(
-      doc => { res.send(doc); },
-      err => { res.status(400).send(err); }
+      doc => res.send(doc),
+      err => res.status(400).send(err)
     );
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo
+    .findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => res.status(400).send());
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -71,9 +97,7 @@ app.delete('/todos/:id', (req, res) => {
 
       res.send({ todo });
     })
-    .catch(err => {
-      res.status(400).send();
-    });
+    .catch(err => res.status(400).send());
 });
 
 app.listen(port, () => {
