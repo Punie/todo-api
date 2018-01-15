279d2d381352ace1b80ba930c7c79b78293269ed
Hugo Saracino <hugo.saracino@gmail.com>
2018-01-15 22:07:22 +0100

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -90,6 +90,17 @@ app.post('/users', (req, res) => {
     .catch(err => res.status(400).send(err));
 });
 
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User
+    .findByCredentials(body.email, body.password)
+    .then(user => user
+      .generateAuthToken()
+      .then(token => res.header('x-auth', token).send(user)))
+    .catch(err => res.status(400).send());
+});
+
 app.get('/users/me', authenticate, (req, res) => res.send(req.user));
 
 app.listen(port, () => {
