7f9cf162d5cfda378e7cd3326cee15564513ce08
Hugo Saracino <hugo.saracino@gmail.com>
2018-01-15 22:37:55 +0100

    Added tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -322,3 +322,27 @@ describe('POST /users/login', () => {
   });
 
 });
+
+describe('DELETE /users/me/token', () => {
+
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        return User
+          .findById(users[0]._id)
+          .then((user) => {
+            expect(user.tokens).to.be.empty;
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+});
